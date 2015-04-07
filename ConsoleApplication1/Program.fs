7ed83142360d846535f64cd0559deca45f639c7c
Matthias Henning <matze_henning@gmx.de>
2015-04-07 15:12:12 +0200

    Next step

--- ConsoleApplication1/Program.fs
+++ ConsoleApplication1/Program.fs
@@ -1,25 +1,34 @@
 ﻿// Learn more about F# at http://fsharp.net
 // See the 'F# Tutorial' project for more help.
+namespace FSharpPlayground
+module first=
 
-let order list sortFunc =
-    List.sortBy sortFunc list
+    open System.Drawing
+    open System
 
-let inline sortFunc x =   x + x
+    let order list sortFunc =
+        List.sortBy sortFunc list
 
+    let inline sortFunc x = x + x
 
-[<EntryPoint>]
-let main argv = 
-    let  a = [-10;2;4]
-    let sorted = order a sortFunc
-    printfn "%A" sorted
+    let dispose (obj:Bitmap) = obj.Dispose()
 
-    let  b = ["-10.0";"2.6";"4.7"]
-    let sorted2 = order b sortFunc
-    printfn "%A" sorted2
+    [<EntryPoint>]
+    let main argv = 
+        let  a = [-10;2;4]
+        let sorted = order a sortFunc
+        printfn "%A" sorted
 
-    let  c = [-10.0;2.6;4.7]
-    let sorted3 = order c sortFunc
-    printfn "%A" sorted3
+//    let  b = ["-10.0";"2.6";"4.7"]
+//    let sorted2 = order b sortFunc
+//    printfn "%A" sorted2
+//
+//    let  c = [-10.0;2.6;4.7]
+//    let sorted3 = order c sortFunc
+//    printfn "%A" sorted3
 
-    0 // return an integer exit code
+        use bitmap = new Bitmap(400,500)
+   // dispose bitmap
+
+        0 // return an integer exit code
 
